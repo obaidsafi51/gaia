c05e827d7e80909ec0be06a692cef23bf0d84086
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-11-29 21:42:27 +0100

    keyframe for animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      animation: {
+        "fade-in": "fade-in 3s ease-in-out forwards",
+        "fade-out": "fade-out 1.5s ease-in-out forwards",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0%",
+          },
+          "50%": {
+            opacity: "50%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        "fade-out": {
+          "0%": {
+            opacity: "100%",
+          },
+          "50%": {
+            opacity: "50%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
       },
     },
   },
